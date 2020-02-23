bd539c1eb985429ec3010b47cb9fdc7fd672b078
rjerue <ryan@jerue.org>
2020-02-22 19:31:04 -0800

    use index

--- fix-html.js
+++ fix-html.js
@@ -5,7 +5,7 @@ const root = "./out";
 function fix(dir) {
   const folders = fs.readdirSync(dir);
   folders.forEach((file, index, list) => {
-    if (file === "index.html" || file === "_next") {
+    if (file === "index.html" || file === "404.html" || file === "_next") {
       return;
     }
     const isDir = fs.statSync(path.join(dir, file)).isDirectory();
@@ -15,14 +15,15 @@ function fix(dir) {
       // is an html file
       const noHTML = file.replace(".html", "");
       console.log(noHTML, dir, file);
-      if (list.includes(noHTML)) {
-        fs.copyFileSync(
-          path.join(dir, file),
-          path.join(dir, noHTML, "index.html")
-        );
-      } else {
-        fs.copyFileSync(path.join(dir, file), path.join(dir, noHTML));
+      const found = path.join(dir, file);
+      const newDir = path.join(dir, noHTML);
+      try {
+        fs.mkdirSync(path.join(dir, noHTML), {});
+      } catch (err) {
+        // exists
       }
+      fs.copyFileSync(found, path.join(dir, noHTML, "index.html"));
+      fs.unlinkSync(found);
     }
   });
 }
