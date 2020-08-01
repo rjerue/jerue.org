8e022aa7124cbae7f22b4e78187651506530b436
Ryan Jerue <ryan@jerue.org>
2020-08-01 14:12:49 -0700

    icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
-            src="https://kit.fontawesome.com/8fb79adcb6.js"
-            crossOrigin="anonymous"
-          />
           <meta
             name="Description"
             content="Website and technical blog of Ryan Jerue, a Software engineer in Southern California"
