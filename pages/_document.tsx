744fc705ddd945ca361be7f386161905db80eacc
Ryan Jerue <ryan@jerue.org>
2021-08-28 13:04:32 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="Description"
-            content="Website and technical blog of Ryan Jerue, a Software engineer in Southern California"
+            content="Website and technical blog of Ryan Jerue, a Software engineer in the United States."
           />
           <meta
             name="keywords"
