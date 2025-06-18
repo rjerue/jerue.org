e823126c488a4e22910cadead2558a2b01fa49d6
Ryan Jerue <ryan@jerue.org>
2025-06-17 20:55:07 -0400

    fix color

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -45,6 +45,7 @@ export function Navbar() {
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
+                  prefetch={name !== "rss"}
                   key={path}
                   href={path}
                   className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
