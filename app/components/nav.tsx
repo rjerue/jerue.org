1da886569e7e8695fd26a9456c2706eb8c15e6b7
Ryan Jerue <ryan@jerue.org>
2025-06-18 09:11:30 -0400

    spacing

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -35,7 +35,7 @@ const NavbarItem = ({
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-2 tracking-tight">
+    <aside className="mb-2 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
@@ -48,7 +48,7 @@ export function Navbar() {
                   prefetch={name !== "rss"}
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 pr-4"
                 >
                   {name}
                 </Link>
