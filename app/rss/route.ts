94361aa25f2e040dae7c1af61a293e2fbd232aa2
Ryan Jerue <ryan@jerue.org>
2025-06-19 17:22:28 -0400

    static

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,6 +1,8 @@
 import { baseUrl } from "app/sitemap";
 import { getBlogPosts } from "app/blog/utils";
 
+export const dynamic = "force-static";
+
 export async function GET() {
   let allBlogs = await getBlogPosts();
 
