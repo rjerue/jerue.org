8563acea75f616e2ea8859bbc5aa219d9876ab54
Ryan Jerue <ryan@jerue.org>
2025-06-19 17:15:17 -0400

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,14 @@
-import { baseUrl } from 'app/sitemap'
+import { baseUrl } from "app/sitemap";
+
+export const dynamic = "force-static";
 
 export default function robots() {
   return {
     rules: [
       {
-        userAgent: '*',
+        userAgent: "*",
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
-  }
+  };
 }
