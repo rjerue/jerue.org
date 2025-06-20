ded893cae25526a701a644c6c2078fc84e1fd571
Ryan Jerue <ryan@jerue.org>
2025-06-19 20:01:44 -0400

    turn off vercel specific telemetry

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
