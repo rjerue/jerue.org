eed42346c9191ed4bcd25dc0a38e3577ff38f630
Ryan Jerue <ryan@jerue.org>
2026-05-03 17:37:45 +0000

    whoami

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,23 @@ export default function Page() {
   return (
     <section>
       <p className="mb-4">
-        I am Ryan Jerue, a Software Engineer in Northern Virginia. My focus is
-        in creating scalable web systems. I can be contacted by email over ryan
-        at jerue dot org.
+        I am Ryan Jerue, a Software Engineer at HubSpot working remotely from Northern Virginia. My
+        focus is in creating scalable web systems. I can be contacted by email over ryan at jerue
+        dot org.
       </p>
-      <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
-        About me:
-      </h2>
+      <h2 className="mb-4 text-2xl font-semibold tracking-tighter">About me:</h2>
       <ul className="list-disc pl-6">
         <li className="mb-2">
-          Previously worked at Amazon, Reify Health, Viasat, Charles River
-          Development, NAVSEA, and with the RIPPLES group at my alma mater, the
-          University of Massachusetts Amherst.
+          Previously worked at Amazon, Reify Health, Viasat, Charles River Development, NAVSEA, and
+          with the RIPPLES group at my alma mater, the University of Massachusetts Amherst.
         </li>
         <li className="mb-2">
-          Enjoy working with JavaScript, TypeScript, Clojure(Script), React,
-          React Native, Java, REST, GraphQL, APIs, SQL, MongoDB, and more.
+          Enjoy working with JavaScript, TypeScript, Clojure(Script), React, React Native, Java,
+          REST, GraphQL, APIs, SQL, MongoDB, and more.
         </li>
         <li className="mb-2">
-          I love photography; hiking and camping; cheering on Liverpool, Red
-          Sox, Patriots; and playing soccer.
+          I love photography; hiking and camping; cheering on Liverpool, the Red Sox, Patriots; and
+          playing soccer.
         </li>
       </ul>
       <div className="my-8">
